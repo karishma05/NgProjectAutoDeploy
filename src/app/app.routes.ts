d137ff02a07a667b1bc8f14740781cc456d2eadd
karishmadas05.kd@gmail.com <karishmadas05.kd@gmail.com>
2025-10-03 12:48:07 +0530

    Added gh-pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { App } from './app';
 
 export const routes: Routes = [
-    { path: '', component: App }, // default route
-  { path: '**', redirectTo: '', pathMatch: 'full' } // wildcard fallback
+    // { path: '', component: App }, // default route
+//   { path: '**', redirectTo: '', pathMatch: 'full' } // wildcard fallback
 
 ];
